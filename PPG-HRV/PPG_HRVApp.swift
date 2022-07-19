48a561fb1a1db2c21435baea1f98e9e29c383cb6
bigshezhang <bigshezhang@gmail.com>
2022-07-19 19:24:08 +0800

    Initial Commit

---
+++ PPG-HRV/PPG_HRVApp.swift
@@ -0,0 +1,17 @@
+//
+//  PPG_HRVApp.swift
+//  PPG-HRV
+//
+//  Created by 李子鸣 on 2022/7/19.
+//
+
+import SwiftUI
+
+@main
+struct PPG_HRVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
