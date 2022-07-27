6dd5b347c233c4680f5c90c60ddc39af130e968a
bigshezhang <bigshezhang@gmail.com>
2022-07-27 17:48:56 +0800

    []

--- PPG-HRV/DeviceManager/ScanDeviceView.swift
+++ PPG-HRV/DeviceManager/ScanDeviceView.swift
@@ -21,16 +21,26 @@ struct ScanDeviceView: View {
     @Environment(\.presentationMode) var presentationMode
     
     func startScan(){
-        ConnectionAdapter.sharedInstance().startScan(false) { error in
-            print("[第一次开启蓝牙扫描是否出错]-> ",error)
-            ScanDevices().transReceive(device)//通过委托模式获取VsDevice实例
-            print("[第一次读到的设备名字] ->",device.name)   //再启动一次蓝牙扫描，这时应该成功启动
-            if device.name != nil{
-                userData.currDevice = device
-                userData.isDeviceConnected = true
-                //冷启动蓝牙扫描，可能尚未powerup
-            }
-            
+        ConnectionAdapter.sharedInstance().startScan(true) { error in
+            print("[启动扫描错误信息 -> ]", error)
+        }
+        
+        while device.name == nil {
+            ScanDevices().transReceive(device)
+        }
+        
+        print("找到了设备")
+        
+//        ConnectionAdapter.sharedInstance().startScan(false) { error in
+//            print("[第一次开启蓝牙扫描是否出错]-> ",error)
+//            ScanDevices().transReceive(device)//通过委托模式获取VsDevice实例
+//            print("[第一次读到的设备名字] ->",device.name)   //再启动一次蓝牙扫描，这时应该成功启动
+//            if device.name != nil{
+//                userData.currDevice = device
+//                userData.isDeviceConnected = true
+//                //冷启动蓝牙扫描，可能尚未powerup
+//            }
+//
 //            DispatchQueue.main.asyncAfter(deadline: .now()+1){
 //                ConnectionAdapter.sharedInstance().startScan(false) { error in
 //                    print("[第二次开启蓝牙扫描是否出错]-> ",error)    //冷启动蓝牙扫描，可能尚未powerup
@@ -45,15 +55,15 @@ struct ScanDeviceView: View {
 //                    startTransfer() //开始传输数据！
 //                }
 //            }
-            
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3){
-                while (device.uuid == nil){
-                      print("尚未获取到设备")
-                  }
-                  print("设备连接完成")
-            }
-        }
+//
+//
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 3){
+//                while (device.uuid == nil){
+//                      print("尚未获取到设备")
+//                  }
+//                  print("设备连接完成")
+//            }
+//        }
     }
     
     func startTransfer(){
